da9b2aaae560ff4904d02e793a7a83088d0cdded
sandmanlukas <41347531+sandmanlukas@users.noreply.github.com>
2022-07-14 20:47:16 +0200

    Update tram_checker.rb

--- Formula/tram_checker.rb
+++ Formula/tram_checker.rb
@@ -1,7 +1,7 @@
 class TramChecker < Formula
   desc "CLI to get coming tram/bus departures in Vastra Gotaland using Västtrafik API."
   homepage "https://github.com/sandmanlukas/Tram-Checker"
-  url "https://github.com/sandmanlukas/Tram-Checker/releases/download/v0.1.1/tram_checker-0.1.1-ARM-apple-monterey.tar.gz"
+  url "https://github.com/sandmanlukas/Tram-Checker/releases/download/v0.1.2/tram_checker-0.1.2-ARM-apple-monterey.tar.gz"
   sha256 "7cad923ac0aa09472d6373e7c36a226227d6542c8c147f8ace64f3e5d1f35589"
   version "0.1.2"
 
